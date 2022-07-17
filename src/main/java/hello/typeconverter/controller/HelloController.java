f406a3d6dc7065fcb4a0cd1c151ea3556b178fbf
정현준 <jeongcode@naver.com>
2022-07-18 08:27:23 +0900

    Initial commit

---
+++ src/main/java/hello/typeconverter/controller/HelloController.java
@@ -0,0 +1,28 @@
+package hello.typeconverter.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello-v1")
+    public String helloV1(HttpServletRequest request , HttpServletResponse response){
+        String data = request.getParameter("data"); // 문자 타입으로 조회
+        Integer integer = Integer.valueOf(data); // 숫자 타입으로 변경
+        log.info("integer = {}" , integer);
+        return "ok";
+    }
+
+    @GetMapping("/hello-v2")
+    public String helloV2(@RequestParam Integer data){
+        log.info("data = {}" , data);
+        return "ok";
+    }
+}
