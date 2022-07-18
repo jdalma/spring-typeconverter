260484afd01fed0d7b3a650b48239686fba4ffa9
정현준 <jeongcode@naver.com>
2022-07-19 08:16:33 +0900

    StirngToIpPort , IpPortToString Converter 예제

--- src/test/java/hello/typeconverter/converter/ConverterTest.java
+++ src/test/java/hello/typeconverter/converter/ConverterTest.java
@@ -1,5 +1,6 @@
 package hello.typeconverter.converter;
 
+import hello.typeconverter.type.IpPort;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -17,5 +18,21 @@ public class ConverterTest {
         IntegerToStringConverter converter = new IntegerToStringConverter();
         String result = converter.convert(10);
         Assertions.assertThat(result).isEqualTo("10");
+    }
+
+    @Test
+    void ipPortToString(){
+        IpPortToStringConverter converter = new IpPortToStringConverter();
+        IpPort ipPort = new IpPort("127.0.0.1", 8080);
+        String result = converter.convert(ipPort);
+        Assertions.assertThat(result).isEqualTo("127.0.0.1:8080");
+    }
+
+    @Test
+    void stringToIpPort(){
+        StringToIpPortConverter converter = new StringToIpPortConverter();
+        String source = "127.0.0.1:8080";
+        IpPort ipPort = converter.convert(source);
+        Assertions.assertThat(ipPort).isEqualTo(new IpPort("127.0.0.1" , 8080));
     }
 }
